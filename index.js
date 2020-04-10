a4d747e00ebc9cc7c96e5a8d6b783528ddfb66aa
Ian McKenzie <imckenzie777@gmail.com>
2020-04-10 02:01:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Susan"
 var height = 39
-var message = `${name} is less than ${height}`
+var message = `${name} is less than ${height} inches tall`
 
 module.exports = {name, height, message}
